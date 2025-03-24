68ff06c1f8d2c15b12dc406a7c6d94c56ebb10b6
phil-ogutu <phil.shifwoko@gmail.com>
2025-03-24 23:12:43 +0300

    Implement buy ticket

--- index.js
+++ index.js
@@ -2,31 +2,45 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("http://localhost:3000/films")
         .then(response => response.json())
         .then(data => {
-            const movieList = document.querySelector(".films")
+            const movieList = document.querySelector(".films");
             const posterImg = document.querySelector(".poster img");
             const movieTitle = document.querySelector(".buy-tickets.header h3");
             const movieDescription = document.querySelector(
-                ".buy-tickets.description p"
+                ".buy-tickets .description p"
             );
             const ticketsRemaining = document.querySelector(
-                ".buy-tickets.description span"
+                ".buy-tickets .description span"
             );
+            const movieShowtime = document.querySelector(".buy-tickets .description button")
+            let selectedFilm = null;
+            const buyButton = document.querySelector(".buy-tickets .btn")
 
             data.forEach((film) => {
                 const listItem = document.createElement("li")
                 listItem.innerHTML = `${film.title}`;
 
-                //Event Listener
+                //Update poster and film when li is selected
                 listItem.addEventListener("click", () => {
+                    selectedFilm = film;
                     posterImg.src = film.poster;
                     movieTitle.textContent = film.title;
                     movieDescription.textContent = film.description;
-                    ticketsRemaining.textContent = `Tickets Remaining: ${film.capacity - film.tickets_sold
-                        }`;
-
+                    ticketsRemaining.textContent = `Tickets Remaining: ${film.capacity - film.tickets_sold}`;
+                    movieShowtime.textContent = film.showtime
                 })
 
-                movieList.appendChild(listItem)
+                movieList.appendChild(listItem);
+            })
+
+            buyButton.addEventListener("click", (event) => {
+                event.preventDefault()
+                let remainingTickets = selectedFilm.capacity - selectedFilm.tickets_sold;
+                if(remainingTickets>0){
+                    selectedFilm.tickets_sold+=1;
+                    ticketsRemaining.textContent = `Tickets Remaining: ${film.capacity - film.tickets_sold}`;
+                }else{
+                    buyButton.textContent = "Sold Out"
+                }
             })
 
         })
